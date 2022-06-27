89c51808d73358c941ea46050fc236e30e1f74df
RambarRambara <afteryou725@gmail.com>
2022-06-28 07:37:20 +0900

    General changes

---
+++ api/src/config.ts
@@ -0,0 +1,9 @@
+export const USER_THUMBNAIL_WIDTH = 60; // Square thumbnail width (60x60)
+export const WORKSPACE_THUMBNAIL_WIDTH = 60; // Square thumbnail width (60x60)
+export const MESSAGE_THUMBNAIL_WIDTH = 400; // Auto thumbnail height (400xauto)
+
+export const USER_PHOTO_MAX_WIDTH = 300; // Square thumbnail width (300x300)
+export const WORKSPACE_PHOTO_MAX_WIDTH = 300; // Square thumbnail width (300x300)
+
+// If a media file is larger than this value (in megabytes), then no thumbnail will be generated.
+export const MAX_FILE_SIZE_MB = 300;
